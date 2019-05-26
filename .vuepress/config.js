e81e9af29dc069ab10ecbbf212683a3545cd5b19
Tomáš Mazač <tomas.mazac@gmail.com>
2019-05-26 18:58:16 +0200

    Přidána favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   dest: "dist",
+  head: [["link", { rel: "icon", href: "/deciduous-tree_1f333.png" }]],
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
