c7d07e4645f0c5b6003e412cd8ba274ae497169f
Tomáš Mazač <tomas.mazac@gmail.com>
2019-05-28 12:19:10 +0200

    Přidán manifest.json

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   dest: "dist",
-  head: [["link", { rel: "icon", href: "/deciduous-tree_1f333.png" }]],
+  head: [
+    ["link", { rel: "icon", href: "/deciduous-tree_1f333.png" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }]
+  ],
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
